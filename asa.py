65f4fd9ab70a13c8b621befe15633201c2c5654e
fengph <jcyhdream@163.com>
2019-06-30 00:05:05 +0800

    1

--- asa.py
+++ asa.py
@@ -5,32 +5,30 @@
 
 import re
 
-asa_conn = "TCP Student  192.168.189.167:32806 Teacher  137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO\n \
-TCP Student  192.168.189.167:80 Teacher  137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO "
+str = """TCP Student  192.168.189.167:32806 Teacher  137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO 
+TCP Student  192.168.189.167:80 Teacher  137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO """
 
-asa_dict = { }
+dict = {}
 
-list = str.split('\n')  # 将字符串转化为list
+list = str.split('\n')
 
-#print(list)
+for i in list:
 
-for conn in asa_conn.split('\n'):
-    re_result = re.match('.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-                   ':(\d{1,5}).*(bytes\s*\d+).*(flags\s*[A-Z]*)',conn).groups()
-    re_result_key = re_result[0],re_result[1],re_result[2],re_result[3]
-    re_result_v = re_result[4],re_result[5]
-    dict[re_result_key] = re_result_v
+    res = re.match('.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+                   ':(\d{1,5}).*(bytes\s*\d+).*(flags\s*[A-Z]*)',i).groups()
+    res_key = res[0],res[1],res[2],res[3]
+    res_v = res[4],res[5]
+    dict[res_key] = res_v
 
 print("打印字典\n")
 
-print(asa_dict)
-# print('\n')
-#
-#
-# print("格式化打印输出\n")
-# for key,value in dict.items():
-#
-#     print('src:{0:<20s}|src_p:{1:<20s}|dst:{2:<20s}|dst_p:{3:<20s}'.format(key[0],key[1],key[2],key[3]))
-#     print('byes:{0:<19s}|flags:{1:<20s}'.format(value[0],value[1]))
-#     print('=' * 110)
-# #采用了新的.format的方法对齐
+print(dict)
+print('\n')
+
+
+print("格式化打印输出\n")
+for key,value in dict.items():
+
+    print(' src:{0:<20s}|src_p:{1:<19s}|dst:{2:<20s}|dst_p:{3:<20s}'.format(key[0],key[1],key[2],key[3]))
+    print('byes:{0:<19s} |flags:{1:<20s}'.format(value[0],value[1]))
+    print('=' * 110)
---
+++ port_list.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# github: https://github.com/fengpenghui
+# 码云: https://gitee.com/fengpenghui0923
+
+
+listB = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+
+print(sorted(listB))
+
+
+# import re
+#
+# port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34',
+#              'eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+#
+#
+#
+# print(sorted(port_list,key=lambda y: y[0]))
