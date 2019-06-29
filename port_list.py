dd492a6585ad3a6cc9b20e4eb05f07c56358c48e
fengph <jcyhdream@163.com>
2019-06-30 00:38:03 +0800

    1

--- port_list.py
+++ port_list.py
@@ -3,17 +3,12 @@
 # github: https://github.com/fengpenghui
 # 码云: https://gitee.com/fengpenghui0923
 
+import re
 
-listB = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
 
-print(sorted(listB))
+port_list.sort(key=lambda x:(int(re.findall(r'\d+',x)[0]),int(re.findall(r'\d+',x)[1]),int(re.findall(r'\d+',x)[2]),int(re.findall(r'\d+',x)[3])))
 
+for a in port_list:
+    print(a)
 
-# import re
-#
-# port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34',
-#              'eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
-#
-#
-#
-# print(sorted(port_list,key=lambda y: y[0]))
